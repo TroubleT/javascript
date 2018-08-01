ea0388bc2b8132ed7a193dc993b30f94ccb58622
Hryhoriy Starykov <starykovhryhoriy@gmail.com>
2018-08-01 10:35:38 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,26 @@
+//const hello = 'Hello World' ;
+//alert(hello);
+
+const userName = prompt('Enter your name');
+const greetings = 'Hello, ' + userName;
+alert(greetings);
+
+const userAge = parseInt(prompt('Enter your age'));
+alert('Your age is ' + userAge);
+
+const nextAge = userAge + 1;
+alert('next year you will be: ' + nextAge);
+
+if (userAge >= 18) {
+    alert('You are adult');
+}
+else {
+    alert('You are child');
+}
+
+function add(a,b) {
+    const result = a + b;
+    return result;
+}
+const result = add(2, 2);
+alert(result);
